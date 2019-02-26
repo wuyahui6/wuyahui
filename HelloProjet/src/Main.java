e18edc93d6e4c9b927ceade48327b2f965b3172b
king <1121195084@qq.com>
2019-02-26 10:32:56 +0800

    规定

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -9,6 +9,6 @@ public class Main {
         System.out.println("第一次修改");
         System.out.println("第二次修改");
         System.out.println("第三次修改");
-        System.out.println("第四次修改");
+        System.out.println("第六次修改");
     }
 }
