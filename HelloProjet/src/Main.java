7d1613ad7af99a04f201db5aa7b1105f95bea81a
king <1121195084@qq.com>
2019-02-26 15:37:23 +0800

    人工费

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -6,5 +6,11 @@ public class Main {
     public void testHelloWrold() {
         System.out.println("HelloWorld!");
         System.out.println("第一次修改");
+        System.out.println("第一次修改");
+        System.out.println("第二次修改");
+        System.out.println("第三次修改");
+        System.out.println("第六次修改");
+        System.out.println("第七次修改");
+        System.out.println("第八次修改");
     }
 }
