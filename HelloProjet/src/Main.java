628690d58e4ccc86884aa1967d64d0a081557bd2
king <1121195084@qq.com>
2019-02-26 10:27:13 +0800

    第三次

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -6,10 +6,7 @@ public class Main {
     public void testHelloWrold() {
         System.out.println("HelloWorld!");
         System.out.println("第一次修改");
-<<<<<<< HEAD
         System.out.println("第一次修改");
-=======
         System.out.println("第二次修改");
->>>>>>> 32c6bc0871fcb39a17d9b5bd316518aba1a8f4ec
     }
 }
